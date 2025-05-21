b6d848aa68d5979ff45a7bb9a93d843ceaec1f46
snk-git-hub <shivanandu.dev@gmail.com>
2025-05-22 03:14:51 +0530

    const  var

--- Common_Programming_Concepts/main.rs
+++ Common_Programming_Concepts/main.rs
@@ -1,8 +1,37 @@
-/*immutable variables*/
 
-// fn main (){
+fn main (){
+    /*immutable variables*/
+
 //     let mut x=5; // mut makes the variable x mutable
 //     println!("value of x is {x}");
 //     x=6;
 //     println!("{x}");
-// }
+
+    /*Constants */
+    // ⚠️constants are values that are bound to a name and are not allowed to change, but there are a few differences between constants and variables.
+    const THREE_HOURS_IN_SECONDS: u16 = 60 * 60 * 3;
+    println!("Three hours in seconds: {THREE_HOURS_IN_SECONDS}");
+    
+
+//     | Type    | Size              | Signed | Range                           |
+// | ------- | ----------------- | ------ | ------------------------------- |
+// | `i8`    | 8-bit             | Yes    | -128 to 127                     |
+// | `u8`    | 8-bit             | No     | 0 to 255                        |
+// | `i16`   | 16-bit            | Yes    | -32,768 to 32,767               |
+// | `u16`   | 16-bit            | No     | 0 to 65,535                     |
+// | `i32`   | 32-bit            | Yes    | -2,147,483,648 to 2,147,483,647 |
+// | `u32`   | 32-bit            | No     | 0 to 4,294,967,295              |
+// | `i64`   | 64-bit            | Yes    | Very large negative to positive |
+// | `u64`   | 64-bit            | No     | Very large                      |
+// | `i128`  | 128-bit           | Yes    | Even larger                     |
+// | `u128`  | 128-bit           | No     | Even larger                     |
+// | `isize` | depends on system | Yes    | For indexing; 32 or 64-bit      |
+// | `usize` | depends on system | No     | For indexing; 32 or 64-bit   
+
+// Floating-Point Types
+// Type	Size	Description
+// f32	32-bit	Single precision float
+// f64	64-bit	Double precision float (default)|
+
+ }
+
